4013ca8c8910e4926dd314f0b506d9b48fbeac0a
Corum Gold <goldcorum@gmail.com>
2022-08-19 11:21:10 -0400

    Add models and associations and link database

--- server/db/index.js
+++ server/db/index.js
@@ -3,13 +3,22 @@ const db = require("./db");
 // require each of your models here...
 const Artist = require("./artist");
 const Album = require("./album");
+const Song = require("./song");
 // ...and give them some nice associations here!
 Album.belongsTo(Artist);
 Artist.hasMany(Album);
 
+Song.belongsTo(Album);
+Album.hasMany(Song);
+
+Artist.hasMany(Song);
+Song.belongsTo(Artist);
+
 module.exports = {
   db,
   Artist,
+  Album,
+  Song,
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
 };
