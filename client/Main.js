82911fecea3213f63f6ebace90dbe80098cd9c6a
Corum Gold <goldcorum@gmail.com>
2022-08-19 12:28:28 -0400

    Make client side reflect mock 1

--- client/Main.js
+++ client/Main.js
@@ -1,9 +1,16 @@
 import React from "react";
+import Sidebar from "./components/Sidebar";
+import AllAlbums from "./components/AllAlbums";
+import Player from "./components/Player";
 
 const Main = () => {
   return (
     <div id="main" className="row container">
-      {/* The music starts here! */}
+      <Sidebar />
+      <div className="container">
+        <AllAlbums />
+      </div>
+      <Player />
     </div>
   );
 };
