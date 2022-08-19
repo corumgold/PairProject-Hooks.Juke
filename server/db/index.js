aa7a8ed6daaf64222f08e4e7a1e3b7ed8d850e19
Corum Gold <goldcorum@gmail.com>
2022-08-19 11:14:56 -0400

    Add models

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,15 @@
-const db = require('./db')
+const Sequelize = require("sequelize");
+const db = require("./db");
 // require each of your models here...
-
+const Artist = require("./artist");
+const Album = require("./album");
 // ...and give them some nice associations here!
+Album.belongsTo(Artist);
+Artist.hasMany(Album);
 
 module.exports = {
   db,
+  Artist,
   // Include your models in your module.exports as well!
   // The seed file expects to find them there!
-}
+};
