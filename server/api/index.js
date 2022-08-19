ec0715b596adb73b58a8e23d209c908665f42db8
Corum Gold <goldcorum@gmail.com>
2022-08-19 12:07:32 -0400

    Add album Id router

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,27 @@
-const router = require('express').Router()
-const { Album } = require('../db/index')
+const router = require("express").Router();
+const { Album, Song, Artist } = require("../db/index");
 
-router.get('/albums', async (req, res, next) => {
-    try {
-        const albums = await Album.findAll()
-        res.send(albums)
-    } catch (err) {
-        next(err)
-    }
-})
+router.get("/albums", async (req, res, next) => {
+  try {
+    const albums = await Album.findAll();
+    res.send(albums);
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+router.get("/albums/:albumId", async (req, res, next) => {
+  try {
+    const album = await Album.findOne({
+      where: {
+        id: req.params.albumId,
+      },
+      include: [Artist, Song],
+    });
+    res.send(album);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
